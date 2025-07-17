398be281a78ae7a21d116a26e26d6675ece0aad2
KrishanuRoyEng <krishanuroygit@gmail.com>
2025-07-17 21:41:16 +0530

    adding product controller and routes

--- backend/controllers/products.controller.js
+++ backend/controllers/products.controller.js
@@ -2,7 +2,16 @@ const fileManager = require("../utils/fileManager");
 const { generateResponse } = require("../utils/helper");
 
 const productController = {
-  getAllProducts: async (req, res) => {},
+  getAllProducts: async (req, res) => {
+    try {
+      let products = await fileManager.readData("products.json");
+      res.json(generateResponse(true, "products retrieved successfully", products));
+    } catch (error) {
+       res
+        .status(500)
+        .json(generateResponse(false, "Failed to fetch products", null, 500));
+    }
+  },
 
   getProductById: async (req, res) => {
     try {
@@ -56,8 +65,61 @@ const productController = {
         .json(generateResponse(false, "Failed to create product", null, 500));
     }
   },
-  updateUsers: async (req, res) => {},
-  deleteUsers: async (req, res) => {},
-  getUserStats: async (req, res) => {},
+  updateProducts: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const updateData = req.body;
+      // Check if email already exists (if updating email)
+      if (updateData.name) {
+        const products = await fileManager.readData("products.json");
+        const existingProduct = products.find(
+          (u) => u.name === updateData.name && u.id === parseInt(id) //! ==
+        );
+
+        if (existingProduct) {
+          return res
+            .status(400)
+            .json(generateResponse(false, "Product already exists", null, 400));
+        }
+      }
+      const updatedProduct = await fileManager.updateData(
+        "products.json",
+        id,
+        updateData
+      );
+      if (!updatedProduct) {
+        return res
+          .status(404)
+          .json(generateResponse(false, "Product not found", null, 404));
+      }
+      res.json(
+        generateResponse(true, "Product updated successfully", updatedProduct, 200)
+      );
+    } catch (error) {
+      res
+        .status(500)
+        .json(generateResponse(false, "Failed to update product", null, 500));
+    }
+  },
+  deleteProducts: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deletedProduct = await fileManager.deleteData("products.json", id);
+
+      if (!deletedProduct) {
+        return res
+          .status(404)
+          .json(generateResponse(false, "Product not found", null, 404));
+      }
+      res.json(
+        generateResponse(true, "Product deleted successfully", deletedProduct)
+      );
+    } catch (error) {
+      res
+        .status(500)
+        .json(generateResponse(false, "Failed to delete product", null, 500));
+    }
+  },
+  getProductStats: async (req, res) => {},
 };
 module.exports = productController;
--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,7 +1,10 @@
 const router= require('express').Router();
 const productController = require('../controllers/products.controller');
 
+router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
 router.post('/', productController.createProducts);
+router.put('/:id', productController.updateProducts);
+router.delete('/:id', productController.deleteProducts);
 
 module.exports = router;
